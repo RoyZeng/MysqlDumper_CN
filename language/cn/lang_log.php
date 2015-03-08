399e900715332ddc1cf5e54782db3507d31e76b4
williamzhao <349544744@qq.com>
2015-03-08 15:05:12 +0800

    add lang cn

---
+++ language/cn/lang_log.php
@@ -0,0 +1,9 @@
+<?php
+$lang['L_LOG_DELETE']="删除日志";
+$lang['L_LOGFILEFORMAT']="日志文件格式";
+$lang['L_LOGFILENOTWRITABLE']="日志文件不可写 !";
+$lang['L_NOREVERSE']="倒序排列";
+$lang['L_REVERSE']="顺序排列";
+
+
+?>
